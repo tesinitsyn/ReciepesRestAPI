0ff982b094dd7c76a54abd32ad2c5dc01db5d3e8
tesinitsyn <sitiev174@gmail.com>
2023-07-22 19:46:35 +0300

    Initial commit

---
+++ src/main/java/com/tesinitsyn/reciepesrestapi/ReciepesRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.tesinitsyn.reciepesrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciepesRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReciepesRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tesinitsyn/reciepesrestapi/ReciepesRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesinitsyn.reciepesrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciepesRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
