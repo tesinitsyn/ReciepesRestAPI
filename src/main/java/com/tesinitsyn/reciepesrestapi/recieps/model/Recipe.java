a5e242ed3d022306d71ba829797c35a1141aa388
tesinitsyn <sitiev174@gmail.com>
2023-07-24 15:58:29 +0300

    Updated implementations for web-scraping(Selenium and Chrome Driver) Created GetAllRecipesService Changed Recipe.id on GenerationType.AUTO

--- src/main/java/com/tesinitsyn/reciepesrestapi/recieps/model/Recipe.java
+++ src/main/java/com/tesinitsyn/reciepesrestapi/recieps/model/Recipe.java
@@ -1,9 +1,6 @@
 package com.tesinitsyn.reciepesrestapi.recieps.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +12,7 @@ public class Recipe {
 
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(name = "ingredients")
---
+++ src/main/java/com/tesinitsyn/reciepesrestapi/recieps/service/GetAllRecipesService.java
@@ -0,0 +1,18 @@
+package com.tesinitsyn.reciepesrestapi.recieps.service;
+
+import com.tesinitsyn.reciepesrestapi.recieps.model.Recipe;
+import com.tesinitsyn.reciepesrestapi.recieps.repository.RecipeRepository;
+
+import java.util.List;
+
+public class GetAllRecipesService {
+    private final RecipeRepository recipeRepository;
+
+    public GetAllRecipesService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public List<Recipe> getAllRecipes(){
+        return recipeRepository.findAll();
+    }
+}
